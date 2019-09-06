f9e56a015ba0b8cbb775c223fcfe37ab843baa15
Serhan Coşkun <grafikri@gmail.com>
2019-09-06 09:48:46 +0300

    env port added

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import axios from "axios"
 import * as express from "express"
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.get("/", (req: any, res: any) => {
   res.send({ state: "OK" })
